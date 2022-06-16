8cccd8aae45c4c4e822e478ec5935ed12858ddad
anand161200 <anandtank99@gmail.com>
2022-06-16 13:54:52 +0530

    sweet alert add in todo

--- task/To_do_list.php
+++ task/To_do_list.php
@@ -11,7 +11,7 @@
         <div class="input-group mt-5">
             <input type="text" id="user_name" class="form-control" >
             <div class="input-group-append">
-                <button class="btn btn-success" onclick="add()" type="button">+ ADD</button>
+                <button class="btn btn-success" onclick="add()" type="button">+</button>
             </div>
         </div>
         <div class="mt-3">
@@ -19,7 +19,11 @@
             </ul>
         </div>
     </div>
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script>
+    
         let user_name = document.getElementById('user_name');
         let user_list = document.getElementById('user_list');
 
@@ -42,36 +46,74 @@
             user_list.innerHTML='';
 
             user_data.forEach(function(user_input, index){
-                user_list.innerHTML+= `<li class="list-group-item d-flex justify-content-between align-items-center">
+                user_list.innerHTML+= `<li class="list-group-item bg-light d-flex justify-content-between align-items-center">
                 <div class="checkbox">
                     <label>
                         <input type="checkbox" id="check_${index}" onclick="Strike(${index})"
                          ${(user_input.strike == true) ? 'checked' : ''}>
-                        <span class="${(user_input.strike == true ) ? 'text-decoration-line-through' : ''}">
+                        <span class="${(user_input.strike == true ) ? 'text-decoration-line-through' : ''} ">
                          ${user_input.name} </span>
                     </label>
                 </div>
-                <button class="btn btn-danger" onclick="remove(${index})">x</button></li>`;   
+                <button class="btn btn-danger btn-sm" onclick="remove(${index},'${user_input.name}')">x</button></li>`;   
             });
         }  
         
         function add() {
-            user_data.push({
-                name : user_name.value,
-                strike: false
-            });
-            reload();
+
+            if(user_name.value == "")
+            {   
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Please Enter your name.'
+                })
+            }
+            else{
+                user_data.push({
+                    name : user_name.value,
+                    strike: false
+                });
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user_name.value} user added successfully`,
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                reload();
                 user_name.value='';
+            }
         }
 
-        function remove(index) {
-            //console.log(index);
-            user_data.splice(index,1);
-            reload();
+        function remove(index,user_input) {
+
+            Swal.fire({
+                title: 'Are you sure want to delete ?',
+                text: `${user_input}`,
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                if (result.isConfirmed) {
+
+                    user_data.splice(index,1);
+                    Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user_input} delete successfully`,
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                    reload();
+                }
+            })
         }
 
         function Strike(index) {
-            let checkBox=document.getElementById(`check_${index}`)
+            let checkBox= document.getElementById(`check_${index}`)
 
             if (checkBox.checked == true){
                user_data[index].strike=true;
@@ -79,7 +121,7 @@
             else{
                 user_data[index].strike=false;
             }
-            reload();    
+            reload();  
         }
 
     </script>  
