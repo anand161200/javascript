7b4eed962cc9684345f4788769b62a6ec95730c2
anand161200 <anandtank99@gmail.com>
2022-06-21 18:38:58 +0530

    dynamic validation

---
+++ task/dynamic_row_input.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <h4 class="text-center mt-3">Student Record</h4>
+    <div class="container bg-light mt-5" style="margin-top: 100px;">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">percentage </th>
+                <th scope="col">Roll-no</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>80%</td>
+                <td>122</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>       
+</body>
+</html>
--- task/dynamic_validation.php
+++ task/dynamic_validation.php
@@ -1,36 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
 <body>
-    <h2> javascript </h2>
-    <div>
-        <div>
-            <input type="text" id="value1" placeholder="Enter first value"><br><br>
-        </div>
-        <div>
-            <input type="text" id="value2" placeholder="Enter first value"><br><br>
-        </div>
-        <div>
-            <input type="text" id="value3" placeholder="Enter first value"><br><br>
-        </div>
-        <button type="submit" onclick="submit()">submit</button>
+    <div class="container bg-light mb-5" style="margin-top: 100px;">
+        <h4 class="text-center mt-1">Form validation</h4>
+        <div class="row">
+            <div class="col-5 offset-4">
+                <div class="col-lg-12">
+                    <label for="user_name" class="form-label">Username</label>
+                    <input type="text" id="value1" data-error="username"
+                     class="form-control required" placeholder="Enter username">
+                </div>
+                <div class="col-lg-12">
+                    <label for="user_name" class="form-label">First name</label>
+                    <input type="text" id="value2" data-error="firstname" 
+                    class="form-control required" placeholder="Enter first name">
+                </div>
+                <div class="col-lg-12">
+                    <label for="user_name" class="form-label">Lastname</label>
+                    <input type="text" id="value3" data-error="lastname"
+                     class="form-control required" placeholder="Enter last name">
+                </div>
+                <br>
+                <button type="submit" class="btn btn-primary" onclick="submit()">Submit</button>
+            </div>
+         </div>
     </div>
+    
     <script> 
-        
-        let filed1= document.getElementById("value1");
-        let filed2= document.getElementById("value2");
-        let filed3= document.getElementById("value3");     
-
+    
         function validation(element)
-        {
-         
+        { 
             if(element.value == "")
             {
-                innerHTML = "please enter value"; 
+                element.nextSibling.remove();
+                let error_message=element.dataset.error;
+                let error_ele= document.createElement("span");
+                error_ele.innerHTML = `Enter ${error_message}`;
+                error_ele.classList.add("text-danger"); 
+                    
+                element.insertAdjacentElement("afterend", error_ele);   
+            }
+            else
+            {
+                element.nextSibling.remove();
             }
         }
+        
         function submit() 
         {
-          validation(filed1,filed2,filed3);
+            let input_filed= document.querySelectorAll(".required");
+
+            input_filed.forEach(function(ele){
+                validation(ele);
+            })
+
         }
     </script>
 </body>
